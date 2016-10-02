cf4ad7de6c27ff1c20ca7e2f8d89f0bbd668c7bc
Eddine <eddine.djerboua@gmail.com>
2016-10-02 22:01:53 +0200

    Test crado

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'devdacticIsAwesome',
-  'database': 'mongodb://localhost/SnT-backend'
+  'database': 'Edd:UnPetitSinge@ds139655.mlab.com:39655/snt'//'mongodb://localhost/SnT-backend'
 };
